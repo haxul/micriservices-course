ad6b5524c2ab2de68b9f6a79b2576c20e09865ae
haxul <s.s.starodubov@yandex.ru>
2020-02-25 23:03:45 +0400

    init

--- usersapp/src/main/java/com/usersapp/usersapp/controllers/UserController.java
+++ usersapp/src/main/java/com/usersapp/usersapp/controllers/UserController.java
@@ -1,15 +1,25 @@
 package com.usersapp.usersapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    private final Environment environment;
+
+    public UserController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status/check")
     public String status() {
-        return "working";
+
+        return "Current port " + environment.getProperty("local.server.port");
     }
 }
