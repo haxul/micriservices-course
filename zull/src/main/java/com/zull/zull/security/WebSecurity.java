b11eaf2dca74fb19ba32561d1e3c05611e65c72a
haxul <s.s.starodubov@yandex.ru>
2020-02-28 20:57:15 +0400

    init secure zuul

---
+++ zull/src/main/java/com/zull/zull/security/WebSecurity.java
@@ -0,0 +1,8 @@
+package com.zull.zull.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+}
